95be31ecfc666d7ce0f57e5f19270e9b943fd36c
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2020-02-24 11:18:19 +0000

    Add util scripts and doc dir

---
+++ utils/scripts/source_tree/build-icc-prof.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+PRJ_ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../" && pwd)"
+SRC_DIR=${1:-$PRJ_ROOT_DIR}
+INSTALL_PREFIX=${2:-../install/}
+
+BMK_CONFIG_FILE="${SRC_DIR}/config/suite_all.txt"
+
+#
+
+# use Intel ICC specific flags
+ICC_FLAGS="${ICC_FLAGS} -O2"
+ICC_FLAGS="${ICC_FLAGS} -ipo"
+ICC_FLAGS="${ICC_FLAGS} -mcmodel=medium"
+ICC_FLAGS="${ICC_FLAGS} -unroll=0"
+#ICC_FLAGS="${ICC_FLAGS} -xT -axT"
+#ICC_FLAGS="${ICC_FLAGS} -xcore2 -axcore2"
+ICC_FLAGS="${ICC_FLAGS} -xcorei7 -axcorei7"
+ICC_FLAGS="${ICC_FLAGS} -profile-functions"
+ICC_FLAGS="${ICC_FLAGS} -profile-loops=all"
+#ICC_FLAGS="${ICC_FLAGS} -static-intel"
+
+C_FLAGS="${CMAKE_C_FLAGS} ${ICC_FLAGS}"
+#C_FLAGS="-g -Wall -O3"
+
+CXX_FLAGS="${CMAKE_CXX_FLAGS} ${ICC_FLAGS}"
+LINKER_FLAGS=""
+
+cmake \
+  -GNinja \
+  -DCMAKE_C_COMPILER=icc \
+  -DCMAKE_CXX_COMPILER=icpc \
+  -DCMAKE_POLICY_DEFAULT_CMP0056=NEW \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_C_FLAGS="${C_FLAGS}" \
+  -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" \
+  -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+  -DHARNESS_BMK_CONFIG_FILE="${BMK_CONFIG_FILE}" \
+  "${SRC_DIR}"
